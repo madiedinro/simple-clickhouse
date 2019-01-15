8857927ba1a553d5ec19b361abab306d3ad04da4
Dmitry Rodin <madiedinro@gmail.com>
2019-01-15 15:23:14 +0300

    Bump version: 0.8.1 → 0.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='simplech',
-    version='0.8.1',
+    version='0.9.0',
     author='Dmitry Rodin',
     author_email='madiedinro@gmail.com',
     license='MIT',
